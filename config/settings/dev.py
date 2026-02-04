ef0a9ce752f613f2024fa6a1b2a4836ea40295b1
Ben Mab <bienvenumabukama@gmail.com>
2026-02-04 22:19:51 +0100

    Use DATABASE_URL in dev settings when available

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -14,7 +14,7 @@ DATABASES = {
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         ssl_require=bool(os.getenv("DATABASE_URL")),
-    }
+    )
 }
 
 DEBUG = True
