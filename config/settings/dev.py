cc59904de7e6e56ae47c1247a0c7c193b466dc0d
Ben Mab <bienvenumabukama@gmail.com>
2026-02-04 22:17:15 +0100

    Use DATABASE_URL in dev settings when available

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -10,9 +10,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=bool(os.getenv("DATABASE_URL")),
     }
 }
 
